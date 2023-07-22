0e78adbf0ef34ff5ad6f2d4ae4e25b7c482f5c48
Curtis Updike <cjupdike@icloud.com>
2023-07-22 14:07:22 -0400

    Proxy API requests

--- ReactUI/src/App.js
+++ ReactUI/src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [hello ,setHello] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.text())
+      .then(data => setHello(data));
+    });
+
   return (
     <div className="App">
       <header className="App-header">
+        {hello && <h1>{hello}</h1>}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
