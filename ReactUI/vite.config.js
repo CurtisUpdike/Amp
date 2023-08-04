1321ed9fe0f872ab8fe93234b3d7f8400442bb91
Curtis Updike <cjupdike@icloud.com>
2023-08-04 08:59:01 -0400

    Restructure

--- ReactUI/vite.config.js
+++ ReactUI/vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    root: "src",
-    build: {
-        outDir: "../dist",
-    },
     server: {
         proxy: {
             "/api": "http://localhost:5073",
