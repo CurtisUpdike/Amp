c3cca22bde01d5b021f8141a5efa242713adb7d9
Curtis Updike <cjupdike@icloud.com>
2023-08-11 09:00:14 -0400

    Repeat playback

--- ReactUI/App/Player/PlaybackControls.jsx
+++ ReactUI/App/Player/PlaybackControls.jsx
@@ -1,19 +1,53 @@
+import { useState } from "react";
 import { Button } from "semantic-ui-react";
 
-const PlayerControls = ({ music }) => (
-    <Button.Group>
-        <Button
-            icon="backward"
-            onClick={async () => await music.skipToPreviousItem()}
-        />
-        <Button icon="play" onClick={async () => await music.play()} />
-        <Button icon="pause" onClick={async () => await music.pause()} />
-        <Button icon="stop" onClick={async () => await music.stop()} />
-        <Button
-            icon="forward"
-            onClick={async () => await music.skipToNextItem()}
-        />
-    </Button.Group>
-);
+const PlayerControls = ({ music }) => {
+    const [repeatMode, setRepeatMode] = useState(0);
+
+    function handleRepeat() {
+        setRepeatMode(() => {
+            const newRepeatMode = (music.repeatMode + 1) % 3;
+            music.repeatMode = newRepeatMode;
+            return newRepeatMode;
+        });
+    }
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                margin: "0 0 1em 0",
+            }}
+        >
+            <Button.Group>
+                <Button
+                    icon="backward"
+                    onClick={async () => await music.skipToPreviousItem()}
+                />
+                <Button icon="play" onClick={async () => await music.play()} />
+                <Button
+                    icon="pause"
+                    onClick={async () => await music.pause()}
+                />
+                <Button icon="stop" onClick={async () => await music.stop()} />
+                <Button
+                    icon="forward"
+                    onClick={async () => await music.skipToNextItem()}
+                />
+            </Button.Group>
+            <Button
+                icon="sync alternate"
+                labelPosition="left"
+                onClick={handleRepeat}
+                content={`Repeat (${
+                    (repeatMode === 0 && "Off") ||
+                    (repeatMode === 1 && "One") ||
+                    (repeatMode === 2 && "All")
+                })`}
+            />
+        </div>
+    );
+};
 
 export default PlayerControls;
