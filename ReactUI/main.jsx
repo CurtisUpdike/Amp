91d5cc257ed47f5c666dba1f016061aff209f478
Curtis Updike <curtis@updike.dev>
2023-08-12 20:27:42 -0400

    Base styles

--- ReactUI/main.jsx
+++ ReactUI/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "semantic-ui-css/semantic.min.css";
+import "./style.css";
 
 async function main() {
     const MusicKit = window.MusicKit;
