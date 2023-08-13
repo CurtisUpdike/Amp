e280baf67c3fcfbf35f93153f2f842d502287235
Curtis Updike <curtis@updike.dev>
2023-08-13 10:11:11 -0400

    NowPlayingDisplay styles

--- ReactUI/App/Player/NowPlayingDisplay.jsx
+++ ReactUI/App/Player/NowPlayingDisplay.jsx
@@ -1,20 +1,16 @@
-import { Header } from "semantic-ui-react";
+import styles from "./NowPlayingDisplay.module.css";
 
 const NowPlayingDisplay = ({ nowPlayingItem, queueIsEmpty }) => {
-    let content;
-    if (!nowPlayingItem || queueIsEmpty) {
-        content = " ";
-    } else {
-        const {
-            attributes: { name, artistName },
-        } = nowPlayingItem;
-        content = name + " - " + artistName;
-    }
-    return (
-        <Header as="h4" block>
-            {content}
-        </Header>
-    );
+  let content;
+  if (!nowPlayingItem || queueIsEmpty) {
+    content = " ";
+  } else {
+    const {
+      attributes: { name, artistName },
+    } = nowPlayingItem;
+    content = name + " - " + artistName;
+  }
+  return <div className={styles.container}>{content}</div>;
 };
 
 export default NowPlayingDisplay;
