8873175452eb3be73141f29e1113185def02f575
Curtis Updike <curtis@updike.dev>
2023-08-22 11:35:29 -0400

    Update heading banners

--- ReactUI/components/Heading/Heading.jsx
+++ ReactUI/components/Heading/Heading.jsx
@@ -2,9 +2,13 @@ import styles from "./Heading.module.css";
 
 const Heading = ({ children }) => (
   <div className={styles.heading}>
-    <div className={styles.banner} />
+    <div className={styles.bannerBorder}>
+      <div className={styles.banner} />
+    </div>
     <h2 className={styles.text}>{children}</h2>
-    <div className={styles.banner} />
+    <div className={styles.bannerBorder}>
+      <div className={styles.banner} />
+    </div>
   </div>
 );
 
