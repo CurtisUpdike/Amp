9f29366079297918a8e3a39fb57cf39d6e5617fd
Curtis Updike <curtis@updike.dev>
2023-09-01 08:49:51 -0400

    Login

--- ReactUI/App/App.tsx
+++ ReactUI/App/App.tsx
@@ -3,12 +3,17 @@ import Player from "./Player";
 import Playlist from "./Playlist";
 import Search from "./Search";
 import styles from "./App.module.css";
+import useAuth from "./useAuth";
+import Login from "./Login";
 
 function App() {
+  const { isAuthorized, toggleAuthorization } = useAuth();
+
   return (
     <div className={styles.app}>
       <Heading>AMP</Heading>
       <Player />
+      {!isAuthorized && <Login login={toggleAuthorization} />}
       <Heading>Playlist</Heading>
       <Playlist />
       <Heading>Search</Heading>
